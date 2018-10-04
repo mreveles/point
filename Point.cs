5ced8e3c12b31d0d3558908b01acc61d43ac7b68
mreveles <mareveles1@buffs.wtamu.edu>
2018-10-04 14:12:26 -0500

    First commit

---
+++ Point.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Point
+{
+    class Point{
+        public int x;
+        int y;
+
+        public Point(){
+            x=0;
+            y=0;
+        }
+
+        public Point(int x0, int y0){
+            x=x0;
+            x=y0;
+        }
+
+        public void display(){
+            Console.Write("({0},{1}) ",x,y);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            Point p2 = new Point(3,6);
+
+            p1.display();
+            p2.display();
+            p1.x=10;
+            p1.display();
+        }
+
+    }
+}
